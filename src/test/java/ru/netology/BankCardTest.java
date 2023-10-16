33108a8631e055451d7f1ca882ffda38b468268a
Nasya Kabakova <kabakova.nas@gmail.com>
2023-10-16 15:11:41 +0300

    EdIt Test succuess form

--- src/test/java/ru/netology/BankCardTest.java
+++ src/test/java/ru/netology/BankCardTest.java
@@ -39,8 +39,8 @@ public class BankCardTest {
     @Test
     void ShouldOpenForm(){
         driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Анастасия");
-        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+9818042544");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Кабакова Анастасия");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79818042544");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
         driver.findElement(By.tagName("button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
